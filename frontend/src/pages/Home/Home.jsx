fc77f350d17116db66f8d0358303905cc32cbe3c
Nebojsa Jovanovic <n.jovanovic@proba.rs>
2019-09-25 10:12:28 +0200

    test za mejl

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ const HomePage = () => {
 		<div className="container mt-4">
 			<h1>Welcome to homepage</h1>
 			<h4>If you are logged in, you can visit members page on <a href="/members">link</a> this</h4>
+			<p>ovo je proba za git</p>
 		</div>
 		</>
 	);
